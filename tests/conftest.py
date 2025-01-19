6dd2546126a6e7791ab3d03121e86a9d510bdb92
Georgy Pomytkin <georgy.pomytkin@yandex.ru>
2025-01-19 14:21:26 +0100

    Modified Urls management to separate file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,16 @@
 import pytest
 
 from selenium import webdriver
+from helpers.urls import Urls
 
 
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
-    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.get(Urls.burger_url_main)
     yield driver
     driver.quit()
+
 
 @pytest.fixture
 def credentials():
@@ -16,4 +18,3 @@ def credentials():
         "email": "georgypomytkin13987@yandex.ru",
         "password": "1234567"
     }
-
